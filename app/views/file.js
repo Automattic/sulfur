27841947d517fb3c964dde1928cad4535591dbcf
Joey Kudish <joachim.kudish@gmail.com>
2014-05-17 11:59:29 +0000

    load single view on image click

--- app/views/file.js
+++ app/views/file.js
@@ -3,6 +3,9 @@ var app = app || {};
 app.fileView = Backbone.View.extend({
 	tagName: 'img',
 	model: app.fileModel,
+	events: {
+		'click' : 'loadSingleView'
+	},
 
 	render: function() {
 		this.$el.attr( 'src', this.model.get( 'link' ) )
@@ -10,5 +13,9 @@ app.fileView = Backbone.View.extend({
 				.attr( 'height', '150' );
 
 		return this;
+	},
+
+	loadSingleView: function() {
+		app.router.navigate( 'view/' + this.model.get( 'id' ), { trigger: true } )
 	}
 });
