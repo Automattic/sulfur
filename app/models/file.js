544b39431b79185a9eaa590cc50e46a51770b548
Jonathan Sadowski <jonathan@sadowski.us>
2014-05-17 14:17:01 +0000

    Add support for deletion to the fileModel
    The URL and method varies from the backbone default pattern, this adds
    support for the URL patterns on the API for deletion

--- app/models/file.js
+++ app/models/file.js
@@ -3,5 +3,19 @@ var app = app || {};
 app.fileModel = Backbone.Model.extend( {
 	url :  function() {
 		return 'https://public-api.wordpress.com/rest/v1/sites/' + app.auth.siteID + '/media/' + this.get( 'id' );
+	},
+
+	destroyUrl: function() {
+		return this.url() + '/delete';
+	},
+
+	destroy: function( options ) {
+		var opts = _.extend(
+			{ 
+				url: this.destroyUrl(),
+				method: 'POST'
+			}, options || {} );
+		
+        return Backbone.Model.prototype.destroy.call( this, opts );
 	}
 } );
