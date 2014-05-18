33493e148d792642a01a3f897b29dacf9ecf241b
Justin Shreve <justin@automattic.com>
2014-05-18 16:55:06 +0000

    Return blogname/URL in the header of the app

--- app/views/userdetails.js
+++ app/views/userdetails.js
@@ -9,8 +9,12 @@ define([
 		template: _.template( $( '#user-template' ).html() ),
 
 		render: function() {
+			var site = this.model.get( 'meta' ).data.site;
+			var data = this.model.attributes;
+			data.blog = site.URL;
+
 			this.$el.html(
-				this.template( this.model.attributes )
+				this.template( data )
 			).attr( 'id', this.id );
 
 			return this;
