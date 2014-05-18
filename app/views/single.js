502bb6946af856fa2f4c4b66c5bc82394d763ee8
Joey Kudish <joachim.kudish@gmail.com>
2014-05-18 16:08:56 +0000

    single view in a modal
    modify the single view template to load in a modal
    
    still needs some styling work and some work to not get rid of the collection in the background

--- app/views/single.js
+++ app/views/single.js
@@ -10,15 +10,17 @@ define([
 		template: _.template( $( '#single-template' ).html() ),
 
 		render: function() {
-			console.log( this.model.attributes );
 			var that = this;
 
 			this.$el.html(
 				this.template( this.model.attributes )
 			);
 
-			this.$el.find( '.single-image-actions .single-image-delete' ).click( function() {
+			this.$el.find( '.modal' ).modal( 'show' );
+
+			this.$el.find( '.btn-danger' ).click( function() {
 				if ( confirm( 'Are you sure you want to delete this image?' ) ) {
+					that.$el.find( '.modal' ).modal( 'hide' );
 					that.model.destroy( {
 						success: function( model, response ) {
 							app.router.navigate( '', { trigger: true } );
