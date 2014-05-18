b12fb43c3d8e6bc4cebd0e9fdd36c5bd631f08bf
Joey Kudish <joachim.kudish@gmail.com>
2014-05-18 17:50:13 +0000

    add formatting for shutter speed, aperture and focal length

--- app/models/file.js
+++ app/models/file.js
@@ -37,6 +37,38 @@ define([
 				}, options || {} );
 
 			return Backbone.Model.prototype.destroy.call( this, opts );
+		},
+
+		parse: function( response ) {
+			response.metadata.image_meta.shutter_speed = this.formatShutterSpeed( response.metadata.image_meta.shutter_speed );
+			response.metadata.image_meta.aperture = 'f/' + response.metadata.image_meta.aperture;
+			response.metadata.image_meta.focal_length = response.metadata.image_meta.focal_length + ' mm';
+			return response;
+		},
+
+		// props Jetpack
+		formatShutterSpeed: function( d ) {
+			if (d >= 1)
+				return Math.round(d) + 's';
+			var df = 1, top = 1, bot = 1;
+			var limit = 1e5; //Increase for greater precision.
+			while (df != d && limit-- > 0) {
+				if (df < d) {
+					top += 1;
+				}
+				else {
+					bot += 1;
+					top = parseInt(d * bot, 10);
+				}
+				df = top / bot;
+			}
+			if (top > 1) {
+				bot = Math.round(bot / top);
+				top = 1;
+			}
+			if (bot <= 1)
+				return '1s';
+			return top + '/' + bot + 's';
 		}
 	} );
 
